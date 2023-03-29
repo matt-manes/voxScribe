e08129e0e28ceb2c1870412a9b5bb975006d4d88
matt manes <mattmanes@pm.me>
2023-03-29 13:21:33 -0500

    chore: add get_text_from_base64 to import in __init__.py.

--- src/voxscribe/__init__.py
+++ src/voxscribe/__init__.py
@@ -1,5 +1,10 @@
-from .voxscribe import (clean_up, get_text_from_MP3, get_text_from_url,
-                        get_text_from_WAV)
+from .voxscribe import (
+    clean_up,
+    get_text_from_MP3,
+    get_text_from_url,
+    get_text_from_WAV,
+    get_text_from_base64,
+)
 
 """ FFMPEG needs to be installed and in your PATH for this module to work. """
 clean_up(max_age=5)
